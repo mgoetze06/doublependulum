7318104ae218d686aa1f371c49376f257f4bd7f4
mgoetze06 <75042211+mgoetze06@users.noreply.github.com>
2021-11-18 16:57:25 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,18 +1,38 @@
+/*function preload(){
+	const data = loadJSON(data.json);
+	console.log(data);
+		let len_one = 160;
+	let len_two = 160;
+	let size = 30;
+	var x0 = width/2;
+	var y0 = height/2;
+	var x1,y1,x2,y2 = 0;
+	var theta1 = PI;
+	var theta2 = PI;
+}*/
+let len_one;
+let len_two;
+let size;
+var x0;
+var y0;
+var x1,y1,x2,y2;
+var theta1,theta2 = 0;
+var dtheta1,dtheta2;
+
 function setup() {
   createCanvas(800, 800);
-
-  
+  stopAnimation();
 }
 
 function draw() {
-  let len_one = 160;
-  let len_two = 160;
-  let size = 30;
-  var x0 = width/2;
-  var y0 = height/2;
-  var x1,y1,x2,y2 = 0;
-  var theta1 = PI;
-  var theta2 = PI;
+  len_one = 160;
+  len_two = 160;
+  size = 30;
+  x0 = width/2;
+  y0 = height/2;
+  x1,y1,x2,y2 = 0;
+  theta1 += dtheta1;
+  theta2 += dtheta2;
   background(220);
   x1 = round(x0 + (len_one * sin(theta1)));
   y1 = round(y0 + (len_one * cos(theta1)));
@@ -24,11 +44,11 @@ function draw() {
   ellipse(x0,y0,size,size);
   ellipse(x1,y1,size,size);
   ellipse(x2,y2,size,size);
-  
 
 }
 
 function startAnimation(){
+	/*
 	let id = null;
 	let pos = 0;
 	clearInterval(id);
@@ -43,4 +63,15 @@ function startAnimation(){
 		  theta1 += 0.11;
 		}
 		}
+		*/
+		
+	dtheta1 += 0.005;
+	dtheta2 += 0.02;
+}
+function stopAnimation(){
+    theta1 = 0;
+	theta2= 0;
+	dtheta1 = 0;
+	dtheta2 = 0;
+	
 }
